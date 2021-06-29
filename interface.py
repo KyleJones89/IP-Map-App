8f2124ec7312baaf89b1dc6f08a7215905cc0b15
KyleJones89 <kyle.jones.389@gmail.com>
2021-06-29 07:44:49 -0400

    commit 2

--- interface.py
+++ interface.py
@@ -1,11 +1,13 @@
 import tkinter
 
+#[+] Initialize Window[+]
 ip_tracker=tkinter.Tk() 
 
+#[+] Create Label [+]
+mainInterfaceLabel = tkinter.Label(ip_tracker, text="IP Tracker")
 
-
-
-
-
+#[+] Pack the Label into the Window [+]
+mainInterfaceLabel.pack()
 
 ip_tracker.mainloop()
+ 
