468ce4d7ff52047f422f363713f2824628ddd262
KyleJones89 <kyle.jones.389@gmail.com>
2021-07-01 16:16:05 -0400

    added geometry to window, made geometry static

--- TestPrograms/entryWidget.py
+++ TestPrograms/entryWidget.py
@@ -1,27 +1,35 @@
-import tkinter
+from tkinter import *
+
+widthofFrame=1280
+heightofFrame=840
+
+#[+] Initialize Window[+]
+ip_tracker=Tk() 
+ip_tracker.geometry(str(widthofFrame) +'x' + str(heightofFrame))
 
 
+e = Entry(ip_tracker, width=50, borderwidth=5)
 
 
+e.pack()
 
 
 def myclick():
-    label1= tkinter.Label(ip_tracker, text="Button Clicked")
+    label1= Label(ip_tracker, text=e.get())
     label1.pack()
 
 
 
-#[+] Initialize Window[+]
-ip_tracker=tkinter.Tk() 
+
 
 #[+] Create Label [+]
-mainInterfaceLabel = tkinter.Label(ip_tracker, text="IP Tracker")
+mainInterfaceLabel = Label(ip_tracker, text="IP Tracker")
 
 
 #[+] Pack the Label into the Window [+]
 mainInterfaceLabel.pack()
 
-mybutton = tkinter.Button(ip_tracker, text= "Click Me!", command=myclick,padx=25 ,pady=25)
+mybutton = Button(ip_tracker, text= "Enter your Name", command=myclick,padx=25 ,pady=25)
 mybutton.pack()
 
 ip_tracker.mainloop()
